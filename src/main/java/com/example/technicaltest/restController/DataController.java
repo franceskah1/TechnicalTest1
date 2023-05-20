93563ba8eaa329cf0887f2326a46b9cf088bedcd
franceskah1 <franceskaharizaj5@gmail.com>
2023-05-20 19:13:31 +0200

    test

--- src/main/java/com/example/technicaltest/restController/DataController.java
+++ src/main/java/com/example/technicaltest/restController/DataController.java
@@ -1,6 +1,9 @@
 package com.example.technicaltest.restController;
+import com.example.technicaltest.exception.NotFoundException;
 import com.example.technicaltest.model.Data;
+import com.example.technicaltest.repository.DataRepository;
 import com.example.technicaltest.service.DataService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,16 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/v1/data")
 public class DataController {
     private final DataService dataService;
-
-    public DataController(DataService dataService) {
+    public DataController(DataService dataService, DataRepository dataRepository) {
         this.dataService = dataService;
+
     }
 
     @PostMapping
     public ResponseEntity<?> insertData(@RequestBody Data data){
-        return dataService.saveData(data);
+        try {
+            dataService.saveData(data);
+            return new ResponseEntity<>("Data inserted successfully!",HttpStatus.OK);
 
-    }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+
+}
 
 
     @GetMapping("/{id}")
@@ -31,8 +41,29 @@ public class DataController {
     }
 
     @PutMapping( "/{id}")
-    public ResponseEntity<?> updateDataById(@PathVariable("id") Long id,@RequestBody Data data) {
-        return dataService.updateData(id, data);
+    public ResponseEntity<?> updateById( @PathVariable Long id,@RequestBody Data data) {
+        try {
+            dataService.updateData(id,data);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch  (NotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> incrementQty(){
+        try {
+            dataService.incrementQty();;
+            return new ResponseEntity<>(HttpStatus.OK);
+        }  catch (Exception e) {
+        e.printStackTrace();
+        return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
     }
 
     }
+}
+
+    
